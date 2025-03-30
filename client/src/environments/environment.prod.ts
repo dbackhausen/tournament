c9ac5b3d71eea5456dd7f16ffbe69f4ba29eda56
Dr. Daniel Backhausen <daniel.backhausen@eightbit.de>
2025-03-30 20:55:44 +0200

    Configuration changes to the client to handle env variables

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: (window as any).env?.API_URL || 'http://localhost:8080/api/'
+  apiUrl: (window as any).env?.API_URL || 'http://localhost:8080/api'
 };
