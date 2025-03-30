5e068e98c7b50de3ab1bf2bd8f95268efa972780
Dr. Daniel Backhausen <daniel.backhausen@eightbit.de>
2025-03-30 21:14:25 +0200

    Solving configuration problems with env variables

--- client/scripts/set-env.js
+++ client/scripts/set-env.js
@@ -1,15 +1,18 @@
 const fs = require('fs');
+const targetPath = './src/assets/env.js';
 
-const envConfig = `
-window.env = {
-  API_URL: "${process.env.API_URL}"
-};
+const envConfigFile = `
+(function (window) {
+  window.env = {
+    API_URL: '${process.env.API_URL || 'http://localhost:8080/api/'}'
+  };
+})(this);
 `;
 
-fs.writeFile('./dist/tournament/browser/assets/env.js', envConfig, (err) => {
+fs.writeFile(targetPath, envConfigFile, function (err) {
   if (err) {
-    console.error('Error writing env.js', err);
+    console.error('Error creating env.js file', err);
   } else {
-    console.log('Environment variables set successfully!');
+    console.log(`env.js file created at ${targetPath}`);
   }
 });
